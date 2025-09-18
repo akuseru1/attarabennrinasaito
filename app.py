b664d19dd9b7aef58f27218e2380d8e97caa6084
アクセル1 <axcelcamp.1@gmail.com>
2025-09-18 18:39:09 +0900

    リッキー先生のかば

--- app.py
+++ app.py
@@ -12,7 +12,15 @@ def index():
 
 @app.route('/tenki')
 def tenki():
-    return render_templ1ate('tenki.html')
+    return render_template('tenki.html')
+
+@app.route('/yotei')
+def yotei():
+    return render_template('yotei.html')
+
+@app.route('/resipi')
+def resipi():
+    return render_template('resipi.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
